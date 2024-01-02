b11b5be3aad002247c0b068f301f9dba1d2768b3
dduy277 <nguyenducduy277@protonmail.com>
2024-01-02 16:55:52 +0700

    first commit

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,80 @@
+import React, { useContext, useState, useEffect } from "react"
+import { auth } from "../firebase"
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+
+const AuthContext = React.createContext()
+
+export function useAuth() {
+  return useContext(AuthContext)
+}
+
+export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState()
+  const [loading, setLoading] = useState(true)
+
+  function signup(email, password) {
+    createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
+      //const user = userCredential.user;
+    })
+      .catch((err) => {
+        console.log(err.code);
+        console.log(err.message);
+      });
+  }
+
+  function login(email, password) {
+    return auth.signInWithEmailAndPassword(auth, email, password)
+  }
+
+  function login(email, password) {
+    signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
+      //const user = userCredential.user;
+    })
+      .catch((err) => {
+        console.log(err.code);
+        console.log(err.message);
+      });
+  }
+
+  function logout() {
+    return auth.signOut()
+  }
+
+  function resetPassword(email) {
+    return auth.sendPasswordResetEmail(email)
+  }
+
+  function updateEmail(email) {
+    return currentUser.updateEmail(email)
+  }
+
+  function updatePassword(password) {
+    return currentUser.updatePassword(password)
+  }
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      setCurrentUser(user)
+      setLoading(false)
+    })
+
+    return unsubscribe
+  }, [])
+
+  const value = {
+    currentUser,
+    login,
+    signup,
+    logout,
+    resetPassword,
+    updateEmail,
+    updatePassword
+  }
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  )
+}
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCABfDvjo_yCAA527UsQIM6_3qvE8pYT3Q",
+  authDomain: "auth-dev-test-ce07b.firebaseapp.com",
+  projectId: "auth-dev-test-ce07b",
+  storageBucket: "auth-dev-test-ce07b.appspot.com",
+  messagingSenderId: "64617145632",
+  appId: "1:64617145632:web:6ca562b516468b581ee345"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
+export default app
