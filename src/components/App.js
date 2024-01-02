3282d7ccdb8a00f68f5de3893bca25d93c306dd2
dduy277 <nguyenducduy277@protonmail.com>
2024-01-03 00:06:36 +0700

    dọn folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import React from "react"
-import Signup from "./Signup"
+import Signup from "./authentication/Signup"
 import { Container } from "react-bootstrap"
 import { AuthProvider } from "../contexts/AuthContext"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Dashboard from "./Dashboard"
-import Login from "./Login"
-import PrivateRoute from "./PrivateRoute"
-import ForgotPassword from "./ForgotPassword"
-import UpdateProfile from "./UpdateProfile"
+import Login from "./authentication/Login"
+import PrivateRoute from "./authentication/PrivateRoute"
+import ForgotPassword from "./authentication/ForgotPassword"
+import UpdateProfile from "./authentication/UpdateProfile"
 
 function App() {
   return (
@@ -19,8 +19,10 @@ function App() {
         <Router>
           <AuthProvider>
             <Routes>
+              {/* Profile/user */}
               <Route exact path="/" element={<PrivateRoute><Dashboard /></PrivateRoute>} ></Route>
               <Route path="/update-profile" element={<PrivateRoute><UpdateProfile /></PrivateRoute>} ></Route>
+              {/* Auth */}
               <Route path="/signup" element={<Signup />} />
               <Route path="/login" element={<Login />} />
               <Route path="/forgot-password" element={<ForgotPassword />} />
