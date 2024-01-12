adcb8f29058126ba3c8741f6e1f251f632a55516
dduy277 <nguyenducduy277@protonmail.com>
2024-01-12 13:23:17 +0700

    add thêm phân quyền của Admin

--- src/components/authentication/Admin/AdminUpdateProfile.js
+++ src/components/authentication/Admin/AdminUpdateProfile.js
@@ -1,57 +1,69 @@
 import React, { useRef, useState } from "react"
 import { Form, Button, Card, Alert } from "react-bootstrap"
-import { useAuth } from "../../../contexts/AuthContext"
-import { Link, useNavigate } from "react-router-dom"
-import { doc, updateDoc } from "firebase/firestore";
+/* import { useAuth } from "../../../contexts/AuthContext" */
+import { Link } from "react-router-dom"
+import { doc, updateDoc, collection, query, where, getDocs } from "firebase/firestore";
 import { db } from '../../../firebase'
 
 export default function UpdateProfile() {
+  const [userID, setUID] = useState();
+  const [confirm, setConfirm] = useState();
+  const [modRef, setmodRef] = useState(false);
   const emailRef = useRef()
-  const passwordRef = useRef()
-  const passwordConfirmRef = useRef()
-  const usernameRef = useRef()
-  const adminRef = useRef()
-  const modRef = useRef()
   const shipperRef = useRef()
-  const { currentUser, updatePassword, updateEmail, updateProfile } = useAuth()
-  const [error, setError] = useState("")
+  /* const { currentUser, updatePassword, updateEmail, updateProfile } = useAuth() */
+  const [error, setError] = useState()
   const [loading, setLoading] = useState(false)
-  const navigate = useNavigate()
+
+  /* useEffect(() => {
+    getUID()
+  }, []) */
 
   function handleSubmit(e) {
     e.preventDefault()
-    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-      return setError("Passwords do not match")
-    }
 
+    function getUID() {
+      const docRef = query(collection(db, "users"), where("Email", "==", emailRef.current.value));
+      getDocs(docRef).then((UID) => {
+        const UIDArray = [];
+        UID.docs.forEach((doc) => {
+          //console.log(doc.id, " => ", doc.data());
+          UIDArray.push({ ...doc.data(), id: doc.id })
+
+        })
+        setUID(UIDArray);
+      })
+    }
+    getUID()
+    /* funtion phải có lỗi setUID(UIDArray), phải click update 2 lần mới đổi userID được*/
     const promises = []
     setLoading(true)
     setError("")
+    if (!userID) {
+      setConfirm("CLICK UPDATE AGAIN TO CONFIRM")
+    } else {
+      setConfirm("Updated")
+      console.log(userID)
+      console.log(userID[0].id)
+      console.log(modRef)
+      if (modRef) {
+        const user = doc(db, `users`, userID[0].id)
+        promises.push(updateDoc(user, {
+          roles: ["mod"]
+        }))
+      }
+      setTimeout(() => {
+        window.location.reload(false);
+      }, 1000)
+    }
 
-    if (usernameRef.current.value) {
-      promises.push(updateProfile(usernameRef.current.value))
-    }
-    if (emailRef.current.value !== currentUser.email) {
-      promises.push(updateEmail(emailRef.current.value))
-    }
-    if (passwordRef.current.value) {
-      promises.push(updatePassword(passwordRef.current.value))
-    }
     /* if (adminRef.current.value) {
       promises.push(updateDoc(doc(db, `users`, currentUser.uid), {
         roles: [adminRef.current.value]
       }))
     } */
-    if (modRef.current.value) {
-      promises.push(updateDoc(doc(db, `users`, currentUser.uid), {
-        roles: [modRef.current.value]
-      }))
-    }
 
     Promise.all(promises)
-      .then(() => {
-        navigate("/user")
-      })
       .catch(() => {
         setError("Failed to update account")
       })
@@ -66,38 +78,15 @@ export default function UpdateProfile() {
         <Card.Body>
           <h2 className="text-center mb-4">Update Profile</h2>
           {error && <Alert variant="danger">{error}</Alert>}
+          {confirm && <><div className='success-msg'>{confirm}</div><br></br></>}
           <Form onSubmit={handleSubmit}>
-            <Form.Group id="username">
-              <Form.Label>Username</Form.Label>
-              <Form.Control
-                type="username"
-                ref={usernameRef}
-                placeholder="Leave blank to keep the same"
-              />
-            </Form.Group>
             <Form.Group id="email">
               <Form.Label>Email</Form.Label>
               <Form.Control
                 type="email"
                 ref={emailRef}
                 required
-                placeholder="User Email"
-              />
-            </Form.Group>
-            <Form.Group id="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                ref={passwordRef}
-                placeholder="Leave blank to keep the same"
-              />
-            </Form.Group>
-            <Form.Group id="password-confirm">
-              <Form.Label>Password Confirmation</Form.Label>
-              <Form.Control
-                type="password"
-                ref={passwordConfirmRef}
-                placeholder="Leave blank to keep the same"
+                placeholder="Email of account that you want to chage role !"
               />
             </Form.Group>
             {['checkbox'].map((type) => (
@@ -114,10 +103,13 @@ export default function UpdateProfile() {
                   type={type}
                   label="mod"
                   id="mod"
-                  ref={modRef}
+                  onChange={(e) => {
+                    setmodRef(e.currentTarget.checked)
+                  }}
                 />
-                <Form.Check
+                <Form.Check /* coi lại sau */
                   inline
+                  disabled
                   type={type}
                   label="shipper"
                   id="shipper"
